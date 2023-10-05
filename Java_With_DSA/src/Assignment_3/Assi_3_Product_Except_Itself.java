91c3f0b52bba089b6415a2e2d4ab74df524c893e
Tamir <tamiralam6393@gmail.com>
2023-10-05 19:16:35 +0530

    java DSA

--- Java_With_DSA/src/Assignment_3/Assi_3_Product_Except_Itself.java
+++ Java_With_DSA/src/Assignment_3/Assi_3_Product_Except_Itself.java
@@ -13,18 +13,18 @@ public class Assi_3_Product_Except_Itself {
 	        for(int i=0;i<arr.length;i++){
 	            arr[i]=scn.nextInt();
 	        }
-	        long[] pre = new long[n];
-	        pre[0] =1;
-	        for(int i=1;i<pre.length;i++){
-	            pre[i] = pre[i-1]*arr[i-1];
+	        long[] left = new long[n];
+	        left[0] =1;
+	        for(int i=1;i<left.length;i++){
+	            left[i] = left[i-1]*arr[i-1];
 	        }
-	        long[] post = new long[n];
-	        post[n-1]=1;
+	        long[] right = new long[n];
+	        right[n-1]=1;
 	        for(int i=n-2;i>=0;i--){
-	            post[i]=post[i+1]*arr[i+1];
+	            right[i]=right[i+1]*arr[i+1];
 	        }
 	        for(int i=0;i<n;i++){
-	            System.out.print(pre[i]*post[i]+ " ");
+	            System.out.print(left[i]*right[i]+ " ");
 	        }
 	    }
 
